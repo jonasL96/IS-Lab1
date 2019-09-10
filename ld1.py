a9eccc0fb8ce5a166824b95e1a7246ca04e17fdb
jonasL96 <47625852+jonasL96@users.noreply.github.com>
2019-09-10 09:57:14 +0300

    Adding some parts of part2 to the previous version

--- ld1.py
+++ ld1.py
@@ -45,7 +45,7 @@ numb_array = numb_array.astype(numpy.float32)#converting everything to float so 
 w1 = random.uniform(-2,1) #random in range of 0 to 1 (can be double)
 w2 = random.uniform(-2,1)
 b = random.uniform(-2,1)
-
+eta = 0.1 # starting eta value
 
 i = 0
 q = 0
@@ -57,21 +57,28 @@ ans2 = results_two()
 
 while i < len(numb_array):
     ans = w1*numb_array[i]+w2*numb_array[i+1]+b
+    #atnaujinami parametrai
     #print(ans) #debug
-    i = i+3
     q = q + 1
     if ans > 0:
         y = 1
+        e = y - numb_array[i+2]
         ans1.insertResult_one(q,indx1) #which elements from data.txt was it
         indx1 = indx1 + 1
         ans1.insertResult_one(ans,indx1) #which elements from data.txt was it
         indx1 = indx1 + 1
     elif ans <= 0:
         y = -1
+        e = y - numb_array[i+2]
         ans2.insertResult_two(q,indx2) #which elements from data.txt was it
         indx2 = indx2 + 1
         ans2.insertResult_two(ans,indx2) #which elements from data.txt was it
         indx2 = indx2 + 1
+    eta = eta * e
+    w1 = w1 + eta*e*numb_array[i]
+    w2 = w2 + eta*e*numb_array[i+1]
+    b = b + eta * e
+    i = i+3
 
 #Results in groups:
 ans1.print_result()
