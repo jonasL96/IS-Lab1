5763222d2625bb0e7ccd4e2f21959c0d42b61477
jonasL96 <47625852+jonasL96@users.noreply.github.com>
2019-09-11 09:36:20 +0300

    Changes in calculation of eta (still incorrect, working on solution)

--- ld1.py
+++ ld1.py
@@ -1,6 +1,8 @@
 import random
 import array
 import numpy
+from sympy import *
+import math
 
 #Results to classes
 class results_one:
@@ -42,10 +44,10 @@ numb_array = numpy.asarray(stri) #converting to numerical array
 numb_array = numb_array.astype(numpy.float32)#converting everything to float so it works
 
 #print(numb_array) #debug
-w1 = random.uniform(-2,1) #random in range of 0 to 1 (can be double)
-w2 = random.uniform(-2,1)
-b = random.uniform(-2,1)
-eta = 0.1 # starting eta value
+w1 = random.uniform(-1,1) #random in range of 0 to 1 (can be double)
+w2 = random.uniform(-1,1)
+b = random.uniform(-1,1)
+eta = 0.01 # starting eta value
 
 i = 0
 q = 0
@@ -57,24 +59,27 @@ ans2 = results_two()
 
 while i < len(numb_array):
     ans = w1*numb_array[i]+w2*numb_array[i+1]+b
-    #atnaujinami parametrai
     #print(ans) #debug
     q = q + 1
+    ans = round(ans,2)
     if ans > 0:
         y = 1
-        e = y - numb_array[i+2]
         ans1.insertResult_one(q,indx1) #which elements from data.txt was it
         indx1 = indx1 + 1
         ans1.insertResult_one(ans,indx1) #which elements from data.txt was it
         indx1 = indx1 + 1
     elif ans <= 0:
         y = -1
-        e = y - numb_array[i+2]
         ans2.insertResult_two(q,indx2) #which elements from data.txt was it
         indx2 = indx2 + 1
         ans2.insertResult_two(ans,indx2) #which elements from data.txt was it
         indx2 = indx2 + 1
-    eta = eta * e
+    e = y - numb_array[i+2]
+    eta_prev = eta
+    eta = limit(y,numb_array[i+2],1)
+    print(eta) #debug
+    if eta > 1 and eta < 0:
+        eta = eta_prev #back to previous value
     w1 = w1 + eta*e*numb_array[i]
     w2 = w2 + eta*e*numb_array[i+1]
     b = b + eta * e
