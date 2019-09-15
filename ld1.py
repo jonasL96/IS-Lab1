82aa746cde66e7ecc219c1d1f3d48db21b97ba43
jonasL96 <47625852+jonasL96@users.noreply.github.com>
2019-09-15 09:41:56 +0300

    Some minor tweaks in code before first lecture

--- ld1.py
+++ ld1.py
@@ -1,8 +1,9 @@
 import random
 import array
 import numpy
-from sympy import *
 import math
+import pandas
+import matplotlib.pyplot as plot
 
 #Results to classes
 class results_one:
@@ -35,16 +36,15 @@ f = open('data.txt', 'r')
 for line in f:
     numbs = f.readlines()
 
+print(numbs)
 #Now to convert the list to a proper array that we can work with
 stri = ''.join(numbs) #converting to string to easily remove comma
-#print(stri) #debug
 stri = stri.replace("\n",",") #changing newline to comma for next step
 stri = stri.split(",")
 numb_array = numpy.asarray(stri) #converting to numerical array
 numb_array = numb_array.astype(numpy.float32)#converting everything to float so it works
-
 #print(numb_array) #debug
-w1 = random.uniform(-1,1) #random in range of 0 to 1 (can be double)
+w1 = random.uniform(-1,1)  #random in range of -1 to 1 (can be double)
 w2 = random.uniform(-1,1)
 b = random.uniform(-1,1)
 eta = 0.01 # starting eta value
@@ -57,8 +57,17 @@ indx2 = 0
 ans1 = results_one()
 ans2 = results_two()
 
-while i < len(numb_array):
-    ans = w1*numb_array[i]+w2*numb_array[i+1]+b
+#splitting the array for perceptron
+#print(numb_array) #pre split
+x1 = numb_array[::3]
+x2 = numb_array[1::3]
+d = numb_array[2::3]
+#print(x1)#debug
+#print(x2)#debug
+#print(d)#debug
+
+while i < len(x1):
+    ans = w1*x1[i]+w2*x2[i]+b
     #print(ans) #debug
     q = q + 1
     ans = round(ans,2)
@@ -74,16 +83,11 @@ while i < len(numb_array):
         indx2 = indx2 + 1
         ans2.insertResult_two(ans,indx2) #which elements from data.txt was it
         indx2 = indx2 + 1
-    e = y - numb_array[i+2]
-    eta_prev = eta
-    eta = limit(y,numb_array[i+2],1)
-    print(eta) #debug
-    if eta > 1 and eta < 0:
-        eta = eta_prev #back to previous value
-    w1 = w1 + eta*e*numb_array[i]
-    w2 = w2 + eta*e*numb_array[i+1]
+    e = d[i] - y
+    w1 = w1 + eta*e*x1[i]
+    w2 = w2 + eta*e*x2[i]
     b = b + eta * e
-    i = i+3
+    i = i+1
 
 #Results in groups:
 ans1.print_result()
