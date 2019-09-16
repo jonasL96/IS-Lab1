5efd1457c3d167ed2c4329724ff238c7571a4920
jonasL96 <47625852+jonasL96@users.noreply.github.com>
2019-09-16 19:34:13 +0300

    Start of rewrite of the program

--- ld1.py
+++ ld1.py
@@ -2,33 +2,7 @@ import random
 import array
 import numpy
 import math
-import pandas
 import matplotlib.pyplot as plot
-
-#Results to classes
-class results_one:
-    def __init__(self):
-        self.results = []
-
-    def insertResult_one(self,result, index):
-        self.results.insert(index,result)
-       # print(self.results) #debug
-
-    def print_result(self):
-        print(self.results)
-
-
-class results_two:
-    def __init__(self):
-        self.results = []
-
-    def insertResult_two(self, result, index):
-        self.results.insert(index,result)
-       # print(self.results) #debug
-
-    def print_result(self):
-        print(self.results)
-
 
 f = open('data.txt', 'r')
 
@@ -47,48 +21,50 @@ numb_array = numb_array.astype(numpy.float32)#converting everything to float so 
 w1 = random.uniform(-1,1)  #random in range of -1 to 1 (can be double)
 w2 = random.uniform(-1,1)
 b = random.uniform(-1,1)
-eta = 0.01 # starting eta value
+eta = 0.1 # starting eta value
 
 i = 0
-q = 0
-indx1 = 0
-indx2 = 0
-
-ans1 = results_one()
-ans2 = results_two()
 
 #splitting the array for perceptron
-#print(numb_array) #pre split
 x1 = numb_array[::3]
 x2 = numb_array[1::3]
 d = numb_array[2::3]
-#print(x1)#debug
-#print(x2)#debug
-#print(d)#debug
+e = []
 
 while i < len(x1):
     ans = w1*x1[i]+w2*x2[i]+b
     #print(ans) #debug
-    q = q + 1
     ans = round(ans,2)
     if ans > 0:
         y = 1
-        ans1.insertResult_one(q,indx1) #which elements from data.txt was it
-        indx1 = indx1 + 1
-        ans1.insertResult_one(ans,indx1) #which elements from data.txt was it
-        indx1 = indx1 + 1
-    elif ans <= 0:
+    else:
         y = -1
-        ans2.insertResult_two(q,indx2) #which elements from data.txt was it
-        indx2 = indx2 + 1
-        ans2.insertResult_two(ans,indx2) #which elements from data.txt was it
-        indx2 = indx2 + 1
-    e = d[i] - y
-    w1 = w1 + eta*e*x1[i]
-    w2 = w2 + eta*e*x2[i]
-    b = b + eta * e
+    e[i] = d[i] - y
     i = i+1
 
-#Results in groups:
-ans1.print_result()
-ans2.print_result()
+e_total = 0
+i = 0
+#Calculating the total error
+while i < len(e)
+	 e_total = abs(e_total) + abs(e[i])
+	 i = i + 1
+
+i = 0
+#perceptron training algorithms
+while e_total != 0:
+    w1 = w1 + eta*e_total*x1[i]
+    w2 = w2 + eta*e_total*x2[i]
+    b = b + eta * e
+    ans = w1*x1[i]+w2*x2[i]+b
+    ans = round(ans,2)
+    if ans > 0:
+        y = 1
+    else:
+        y = -1
+    e[i] = d[i] - y
+    
+    
+
+
+
+	 
